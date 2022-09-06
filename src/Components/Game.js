834ae59ac62ac9ff13aacc65dd90e0c1c4923b04
Parker Fisher <pfishe13@kent.edu>
2022-09-06 13:11:55 -0400

    Fix bug where game would not restart if score was less than highscore

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,27 +1,29 @@
-import { click } from '@testing-library/user-event/dist/click';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import CardContainer from './CardContainer';
 import Scoreboard from './Scoreboard';
 
 function Game() {
   const [score, setScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
-
   const [clickedArray, setClickedArray] = useState([]);
 
   function cardClicked(e) {
     const clicked = e.target.id;
     const alreadyClicked = checkMatch(clicked);
     if (!alreadyClicked) {
-      console.log('Adding', clicked);
       addToClickedArray(clicked);
       setScore(score + 1);
     } else {
-      if (score > highScore) {
-        setHighScore(score);
-        setScore(0);
-      }
+      resetScore();
     }
+  }
+
+  function resetScore() {
+    if (score > highScore) {
+      setHighScore(score);
+    }
+    setScore(0);
+    setClickedArray([]);
   }
 
   function addToClickedArray(name) {
@@ -30,9 +32,7 @@ function Game() {
 
   function checkMatch(name) {
     const indexFound = clickedArray.find((item) => item === name);
-    console.log('Index found', indexFound);
-    if (indexFound === undefined) return false;
-    return true;
+    return indexFound === undefined ? false : true;
   }
 
   return (
