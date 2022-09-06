d6a79ecdf7a0974c4e13754da699cdd3c5a84daa
Parker Fisher <pfishe13@kent.edu>
2022-09-06 14:33:11 -0400

    UI update with colors and hover/click effects

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,6 +4,7 @@ function Card({ name, cardClicked }) {
   return (
     <div className="card" onClick={cardClicked}>
       <img id={name} alt="" src={'/images/' + name + '.png'}></img>
+      <span></span>
     </div>
   );
 }
--- src/Components/Scoreboard.js
+++ src/Components/Scoreboard.js
@@ -2,10 +2,16 @@ import React, { useState } from 'react';
 
 function Scoreboard({ score, highScore }) {
   return (
-    <>
-      <h1>Score:{score}</h1>
-      <h1>High Score:{highScore}</h1>
-    </>
+    <div className="header">
+      <div>
+        <h1>Cavs Memory Game</h1>
+        <h4>Don't select a Cav more than once!</h4>
+      </div>
+      <div>
+        <h2>Score:{score}</h2>
+        <h2>High Score:{highScore}</h2>
+      </div>
+    </div>
   );
 }
 
